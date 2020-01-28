1311c3697b5d846f66159839fb81e89a0d04602c
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-01-28 13:51:22 -0500

    initiating with inquirer and mySQL connection

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "employee_tracker_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
