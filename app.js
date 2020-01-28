9bdef8af84efbd2315a7fb797a13eeb4306a41e2
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-01-28 16:33:20 -0500

    haadder clours ADJM

--- app.js
+++ app.js
@@ -21,11 +21,13 @@ CFonts.say('EMPLOYEE TRACKER', {
     align: 'left',              // define text alignment
     colors:  ['cyanBright', 'whiteBright','redBright'],         // define all colors
     background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
-    letterSpacing: 1,           // define letter spacing
-    lineHeight: 1,              // define the line height
+    letterSpacing:0 ,           // define letter spacing
+    lineHeight: 0,              // define the line height
     space: true,                // define if the output text should have empty lines on top and on the bottom
     maxLength: '0',             // define how many character can be on one line
 });
+
+
 
 connection.connect(function(err) {
   if (err) {
